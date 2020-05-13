9bf28b2223d4ef873fce47f5cbfa1757b9ef5e99
Adrian Pietruszka <48991330+Pietrov98@users.noreply.github.com>
2020-05-13 16:18:19 +0200

    Template of web page for user adding

---
+++ src/main/java/com/Pietrov98/chatwebapp/ChatWebappApplication.java
@@ -0,0 +1,12 @@
+package com.Pietrov98.chatwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Pietrov98/chatwebapp/ChatWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pietrov98.chatwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
